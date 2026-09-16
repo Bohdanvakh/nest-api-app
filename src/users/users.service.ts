140845b4c8b22ddd0686b8f255315bc677ea6898
bohdanvakh <bohdanvakh@gmail.com>
2026-09-16 20:56:21 +0300

    feat(users): add CRUD methods for users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -20,6 +20,27 @@ export class UsersService {
 
     // GET: index
     find(email: string) {
-        return this.repo.find({ where: { email } });
+        return this.repo.findBy({ email });
+    }
+
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.findOne(id);
+
+        if(!user) {
+            throw new Error('user not found!');
+        }
+
+        Object.assign(user, attrs);
+        return this.repo.save(user);
+    }
+
+    async remove(id: number) {
+        const user = await this.findOne(id);
+
+        if(!user) {
+            throw new Error('user not found');
+        }
+
+        return this.repo.remove(user);
     }
 }
