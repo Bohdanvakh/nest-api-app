f2b2dd2219816f598b4a67939f3020f468d860ba
bohdanvakh <bohdanvakh@gmail.com>
2026-09-26 00:39:15 +0300

    feat(users): add users creation and password hashing to the signup method

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,5 +1,9 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
 import { UsersService } from "./users.service";
+import { randomBytes, scrypt as _scrypt } from "crypto";
+import { promisify } from "util";
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -8,13 +12,26 @@ export class AuthService {
     async signup(email: string, password: string) {
         // check if email in use
         const users = await this.usersService.find(email);
-        // hash the user password
+
         if(users.length) {
             throw new BadRequestException('Email in use');
         }
+
+        // hash the user password
+        // generate a salt
+        const salt = randomBytes(8).toString('hex');
+
+        // hash the salt and password together
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+        // join the hashed result and the salt together
+        const result = salt + '.' + hash.toString('hex');
+
         // create a new user and save it
+        const user = await this.usersService.create(email, result);
 
         // return the user
+        return user;
     }
 
     signin() {
